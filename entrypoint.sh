ec46fd7b9b8878eefe58311cb0cbf5c9e690261c
tom <tom.bevan@r3labs.io>
2016-06-30 18:14:25 +0100

    fixed entrypoint wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Waiting for Postgres"
-while true; do
-    nc -q 1 postgres 5432 2>/dev/null && break
-done
+while ! echo exit | nc postgres 5432; do sleep 1; done
 
 echo "Starting datacenter-store"
 /go/bin/datacenter-store
