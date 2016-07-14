b2d37f2cc42bd576c568e52daea13d92b8df07e3
Raül Pérez <repejota@gmail.com>
2016-07-14 15:29:16 +0000

    Added tests for aws and vcloud
    Signed-off-by: Raül Pérez <repejota@gmail.com>

---
+++ vcloud_test.go
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/nats-io/nats"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestVcloudDatacenter(t *testing.T) {
+	setupNats()
+	n.Subscribe("config.get.postgres", func(msg *nats.Msg) {
+		n.Publish(msg.Reply, []byte(`{"names":["users","datacenters","datacenters","services"],"password":"","url":"postgres://postgres@127.0.0.1","user":""}`))
+	})
+	setupPg()
+	startHandler()
+
+	Convey("Scenario: creating a vcloud datacenter", t, func() {
+		setupTestSuite()
+		Convey("Given i create a datacenter with type vcloud", func() {
+			Convey("Then it should return the created record and it should be stored on DB", func() {
+				msg, err := n.Request("datacenter.set", []byte(`{"name":"vcloudDC", "type":"vcloud"}`), time.Second)
+				output := Entity{}
+				output.LoadFromInput(msg.Data)
+				So(output.ID, ShouldNotEqual, nil)
+				So(output.Name, ShouldEqual, "vcloudDC")
+				So(output.Type, ShouldEqual, "vcloud")
+				So(err, ShouldEqual, nil)
+			})
+		})
+	})
+}
