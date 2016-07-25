b1ea74043e453fe943fbc90c412e15e5a4a1cb1d
Tom Bevan <tom.bevan@r3labs.io>
2016-07-25 16:26:36 +0100

    updated dockerfile and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+echo "Waiting for NATS"
+while ! echo exit | nc postgres 4222; do sleep 1; done
+
 echo "Waiting for Postgres"
 while ! echo exit | nc postgres 5432; do sleep 1; done
 
